5f9f0ea786163eeab9c36d5a22c60390e01093e3
kuahbanyak <kualisabi@gmail.com>
2022-06-30 10:41:16 +0700

    first commit

---
+++ src/Domain/Entities/Detail.cs
@@ -0,0 +1,16 @@
+namespace CleanArchitecture.Domain.Entities;
+
+public class Detail
+{
+    public int Id {get; set;}
+
+    public string JenisBahanBakar {get; set;}
+
+    public string JumlahTempatDuduk {get; set;}
+
+    public string KunciCadangan {get; set;}
+
+    public string GaransiPabrik {get;set;}
+
+    public string 
+}
